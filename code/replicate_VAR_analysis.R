b021ad637f215f89944ba6dcc0a78cad6cf53439
matdehaven <matthew_dehaven@brown.edu>
2022-11-01 09:59:33 -0400

    Found a couple data cleaning errors, got the rotations to successfully work!

--- code/replicate_VAR_analysis.R
+++ code/replicate_VAR_analysis.R
@@ -14,53 +14,56 @@ require(vars)
 date_range <- ymd(c("1983-01-01", "2017-12-31"))
 
 check_signs <- function(X){
-  res <- rep(FALSE,26)
+  sigma <- abs(diag(diag(X),4,4))
+  A <- X %*% solve(sigma)
+  
+  res <- rep(FALSE,26) 
   
   ## Direct Impulse restrictions
-  if(X[1,1] >  0) res[1] <- TRUE
-  if(X[2,1] >  0) res[2] <- TRUE
-  if(X[3,1] >  0) res[3] <- TRUE
-  if(X[4,1] >  0) res[4] <- TRUE
+  if(A[1,1] >  0) res[1] <- TRUE
+  if(A[2,1] >  0) res[2] <- TRUE
+  if(A[3,1] >  0) res[3] <- TRUE
+  if(A[4,1] >  0) res[4] <- TRUE
   
-  if(X[1,2] >  0) res[5] <- TRUE
-  if(X[2,2] >  0) res[6] <- TRUE
-  if(X[3,2] >  0) res[7] <- TRUE
-  if(X[4,2] <=  0) res[8] <- TRUE
+  if(A[1,2] >  0) res[5] <- TRUE
+  if(A[2,2] >  0) res[6] <- TRUE
+  if(A[3,2] >  0) res[7] <- TRUE
+  if(A[4,2] <=  0) res[8] <- TRUE
   
-  if(X[1,3] <= 0) res[9] <- TRUE
-  if(X[2,3] <= 0) res[10] <- TRUE
-  if(X[3,3] <= 0) res[11] <- TRUE
-  if(X[4,3] <= 0) res[12] <- TRUE
+  if(A[1,3] <= 0) res[9] <- TRUE
+  if(A[2,3] <= 0) res[10] <- TRUE
+  if(A[3,3] <= 0) res[11] <- TRUE
+  if(A[4,3] <= 0) res[12] <- TRUE
   
-  if(X[1,4] >  0) res[13] <- TRUE
-  if(X[2,4] >  0) res[14] <- TRUE
-  if(X[3,4] >  0) res[15] <- TRUE
-  if(X[4,4] <= 0) res[16] <- TRUE
+  if(A[1,4] >  0) res[13] <- TRUE
+  if(A[2,4] >  0) res[14] <- TRUE
+  if(A[3,4] >  0) res[15] <- TRUE
+  if(A[4,4] <= 0) res[16] <- TRUE
   
   ## Between Asset Restrctions
   ## Growth Shock
-  if(abs(X[1,1]) > abs(X[3,1])) res[17] <- TRUE
-  if(abs(X[2,1]) > abs(X[3,1])) res[18] <- TRUE
+  if(abs(A[1,1]) > abs(A[3,1])) res[17] <- TRUE
+  if(abs(A[2,1]) > abs(A[3,1])) res[18] <- TRUE
   
   ## Monetary Policy Shock
-  if(abs(X[1,2]) > abs(X[2,2])) res[19] <- TRUE
-  if(abs(X[2,2]) > abs(X[3,2])) res[20] <- TRUE
+  if(abs(A[1,2]) > abs(A[2,2])) res[19] <- TRUE
+  if(abs(A[2,2]) > abs(A[3,2])) res[20] <- TRUE
   
   ## Hedging Risk Premium Shock
-  if(abs(X[1,3]) < abs(X[2,3])) res[21] <- TRUE
-  if(abs(X[2,3]) < abs(X[3,3])) res[22] <- TRUE
+  if(abs(A[1,3]) < abs(A[2,3])) res[21] <- TRUE
+  if(abs(A[2,3]) < abs(A[3,3])) res[22] <- TRUE
   
   ## Common Risk Premium Shock
-  if(abs(X[1,4]) < abs(X[2,4])) res[23] <- TRUE
-  if(abs(X[2,4]) < abs(X[3,4])) res[24] <- TRUE
+  if(abs(A[1,4]) < abs(A[2,4])) res[23] <- TRUE
+  if(abs(A[2,4]) < abs(A[3,4])) res[24] <- TRUE
   
   ## Within Asset Restrictions
   ## Two-year Yield
   if(X[1,1]**2 + X[1,2]**2 > X[1,3]**2 + X[1,4]**2) res[25] <- TRUE
-  
-  ## Ten-year Yield
+   
+  # ## Ten-year Yield
   if(X[3,1]**2 + X[3,2]**2 < X[3,3]**2 + X[3,4]**2) res[26] <- TRUE
-  
+   
   return(res)
 } 
 
@@ -75,7 +78,9 @@ series <- c("yc2", "yc5", "yc10", "sp")
 X <- as.matrix(na.omit(data[date %between% date_range, ..series]))
 
 ## Demean the data
-X <- X - apply(X, 2, mean) 
+X <- X - matrix(rep(colMeans(X), nrow(X)), ncol = 4, byrow = T)
+
+
 
 ## Find the BIC for lag length (should be 1)
 ## SC(n) is the BIC number
@@ -83,8 +88,8 @@ IC <- vars::VARselect(X)$selection
 print(IC)
 
 ## Set up reduced form VAR
-svars::id.chol(myvar)
-myvar <- vars::VAR(X, type = "const", ic = "SC")
+## type = "none" means there is no beta constant
+myvar <- vars::VAR(X, type = "none", ic = "SC")
 
 ## Grab the variance-covariance matrix of reduced form shocks
 u <- residuals(myvar)
@@ -93,63 +98,90 @@ Omega <- cov(u)
 ## Construct a Lower Choleskey Matrix
 P <- t(chol(Omega))
 
-## Can back out the orthoganalized shocks
-ws <- u %*% solve(t(P))
+## Can back out the orthoganalized shocks, omega
+omega <- u %*% solve(t(P))
+omega2 <- t(solve(P) %*% t(u))
 
 ## Note that cov-var of orthogonalized shocks is roughly I
-cov(ws) |> round(4)
+cov(omega) |> round(4)
+cov(omega2) |> round(4)
+
 
 ## Generate Rotation matrixes, Q
 a <- matrix(rnorm(16),4,4)
-Q <- qr.Q(qr(a))
 
-## Cheack that QQ' = I
-Q %*% t(Q) |> round(4)
+## Decompose into Qr parts
+Q <- qr.Q(qr(a))
+R <- qr.R(qr(a))
+
+## Make sure the diagonal of r is all positive
+R2 <- diag(sign(diag(R)),4,4) %*% R
+Q2 <- Q %*% solve(diag(sign(diag(R)),4,4))
 
 ## Rotate the Choleskey matrix
-R <- P %*% Q
-
-## splitting like the appendix does
-sigma <- abs(diag(diag(R),4,4))
-R2 <- R %*% solve(sigma)
-
-R2
-
-## Back out impolied shocks
-ws_R <- u %*% solve(t(R))
+A_tilde <- P %*% t(Q2)
 
 ## Check if it matches sign restrictions
-check_signs(R)
+check_signs(A_tilde)
 
-set.seed(13805)
-i = 0
-maxiter = 10^7
-t <- rbindlist(lapply(1:maxiter, function(i){
-  if(i %% 10^4 == 0) print(i)
+## We need to search a large number of possible rotation matrixes Q
+## To find ones that match all of our sign restrictions
+seed <- 13805
+maxiter = 10^6
+for(i in 1:maxiter){
+  if(i %% 10^6 == 0) print(i/10^6)
+  set.seed(seed + i)
   
   a <- matrix(rnorm(16),4,4)
+  
   Q <- qr.Q(qr(a))
+  R <- qr.R(qr(a))
   
-  ## Rotate the Choleskey matrix
-  R <- P %*% t(Q)
+  R2 <- diag(sign(diag(R)),4,4) %*% R
+  Q2 <- Q %*% solve(diag(sign(diag(R)),4,4))
+
+  A_tilde <- P %*% t(Q2)
+  signs <- check_signs(A_tilde)
   
-  ## Check if it matches sign restrictions
-  data.table(t(check_signs(R)))
-}))
+  if(all(signs)){
+    print(paste0("Success on ", i))
+    saveRDS(list(
+      check_signs = signs,
+      seed = seed + i,
+      a = a,
+      P = P,
+      Q = Q2,
+      A_tilde = A_tilde
+    ), file = paste0("./output/VAR_rotations/rotation_", i, ".RDS"))
+  }
+}
 
-apply(t,2,function(x){sum(x)/length(x)})
+for(i in 1:10){
+  print(i)
+  t <- rbindlist(lapply(1:10^5, function(i){
+    # if(i %% 10^4 == 0) print(i/10^4)
+    
+    a <- matrix(rnorm(16),4,4)
+    
+    Q <- qr.Q(qr(a))
+    R <- qr.R(qr(a))
+    
+    R2 <- diag(sign(diag(R)),4,4) %*% R
+    Q2 <- Q %*% solve(diag(sign(diag(R)),4,4))
+    
+    A_tilde <- P %*% t(Q2)
+    
+    data.table(t(check_signs(A_tilde)))
+  }))
+
+print(apply(t,2,function(x){round(sum(x)/length(x),2)}))
+print(table(apply(t,1,function(x){sum(x)})))
+t[apply(t,1,function(x){sum(x)}) == 24,]
+t[apply(t,1,function(x){sum(x)}) == 26,]
+
+}
 
 
-a <- matrix(rnorm(16),4,4)
-qr(a)
-
-b <- matrix(rnorm(16),4,4)
-t <- qr(b)
-t(t$qr) %*% (t$qr)
-qr.Q(t)
-qr.R(t)
-qr.Q(t) %*% t(qr.Q(t)) |> round(4)
-
-
-
-
+check_signs(P)
+check_signs(P %*% Q)
+check_signs(P %*% Q %*% Q %*% Q)
